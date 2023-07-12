bacc236b7a99f41504c1efb16f07a86f202bc7ad
AyaTareekAhmmeed <91810219+AyaTareekAhmmeed@users.noreply.github.com>
2023-07-12 06:49:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import subprocess
+import logging, ngrok
+
+logging.basicConfig(level=logging.INFO)
+tunnel = ngrok.werkzeug_develop()
 
 app = Flask(__name__)
 CORS(app)
+
 
 def preprossing(image):
     img_array = np.frombuffer(image.read(), np.uint8)
@@ -29,28 +34,40 @@ def preprossing(image):
 def index():
     return "Welcome to the Eye Disease Prediction API"
 
+
 @app.route('/predictApi', methods=["POST"])
 def api():
     try:
         if 'fileup' not in request.files:
             return jsonify({'Error': 'Please try again. The Image does not exist.'})
         image = request.files.get('fileup')
-        
+
         result = preprossing(image)
-       
+
         return jsonify({'prediction': result})
     except:
         return jsonify({'Error': 'Error occurred during prediction.'})
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         image = request.files.get('fileup')
-        
+
         result = preprossing(image)
         return jsonify({'prediction': result})
     except:
         return jsonify({'Error': 'Error occurred during prediction.'})
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run()
+
+# Start Ngrok tunnel
+ngrok_process = subprocess.Popen(['ngrok', 'http', '5000'])
+
+try:
+    app.run()
+finally:
+    # Terminate Ngrok tunnel when Flask app is stopped
+    ngrok_process.terminate()
